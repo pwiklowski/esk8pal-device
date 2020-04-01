0963e54a7b3c0ac9f98482ede852986cedef1d7f
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2020-04-01 18:47:42 +0200

    Initial commit with slightly modified sd card example

---
+++ main/main.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_vfs_fat.h"
+#include "driver/sdmmc_host.h"
+#include "driver/sdspi_host.h"
+#include "sdmmc_cmd.h"
+
+static const char *TAG = "esk8";
+
+void init_sd()
+{
+  ESP_LOGI(TAG, "Initializing SD card");
+
+  ESP_LOGI(TAG, "Using SDMMC peripheral");
+  sdmmc_host_t host = SDMMC_HOST_DEFAULT();
+
+  sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
+
+  gpio_set_pull_mode(15, GPIO_PULLUP_ONLY); // CMD, needed in 4- and 1- line modes
+  gpio_set_pull_mode(2, GPIO_PULLUP_ONLY);  // D0, needed in 4- and 1-line modes
+  gpio_set_pull_mode(4, GPIO_PULLUP_ONLY);  // D1, needed in 4-line mode only
+  gpio_set_pull_mode(12, GPIO_PULLUP_ONLY); // D2, needed in 4-line mode only
+  gpio_set_pull_mode(13, GPIO_PULLUP_ONLY); // D3, needed in 4- and 1-line modes
+
+  esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+      .format_if_mount_failed = false,
+      .max_files = 5,
+      .allocation_unit_size = 16 * 1024};
+
+  sdmmc_card_t *card;
+  esp_err_t ret = esp_vfs_fat_sdmmc_mount("/sdcard", &host, &slot_config, &mount_config, &card);
+
+  if (ret != ESP_OK)
+  {
+    if (ret == ESP_FAIL)
+    {
+      ESP_LOGE(TAG, "Failed to mount filesystem. "
+                    "If you want the card to be formatted, set format_if_mount_failed = true.");
+    }
+    else
+    {
+      ESP_LOGE(TAG, "Failed to initialize the card (%s). "
+                    "Make sure SD card lines have pull-up resistors in place.",
+               esp_err_to_name(ret));
+    }
+    return;
+  }
+
+  sdmmc_card_print_info(stdout, card);
+}
+
+void app_main(void)
+{
+  init_sd();
+
+  // First create a file.
+  ESP_LOGI(TAG, "Opening file");
+  FILE *f = fopen("/sdcard/hello.txt", "w");
+  if (f == NULL)
+  {
+    ESP_LOGE(TAG, "Failed to open file for writing");
+    return;
+  }
+  fprintf(f, "Hello !\n");
+  fclose(f);
+  ESP_LOGI(TAG, "File written");
+
+  // Check if destination file exists before renaming
+  struct stat st;
+  if (stat("/sdcard/foo.txt", &st) == 0)
+  {
+    // Delete it if it exists
+    unlink("/sdcard/foo.txt");
+  }
+
+  // Rename original file
+  ESP_LOGI(TAG, "Renaming file");
+  if (rename("/sdcard/hello.txt", "/sdcard/foo.txt") != 0)
+  {
+    ESP_LOGE(TAG, "Rename failed");
+    return;
+  }
+
+  // Open renamed file for reading
+  ESP_LOGI(TAG, "Reading file");
+  f = fopen("/sdcard/foo.txt", "r");
+  if (f == NULL)
+  {
+    ESP_LOGE(TAG, "Failed to open file for reading");
+    return;
+  }
+  char line[64];
+  fgets(line, sizeof(line), f);
+  fclose(f);
+  // strip newline
+  char *pos = strchr(line, '\n');
+  if (pos)
+  {
+    *pos = '\0';
+  }
+  ESP_LOGI(TAG, "Read from file: '%s'", line);
+
+  // All done, unmount partition and disable SDMMC or SPI peripheral
+  esp_vfs_fat_sdmmc_unmount();
+  ESP_LOGI(TAG, "Card unmounted");
+}
