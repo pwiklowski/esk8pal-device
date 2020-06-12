d6d706d10275776d81c9ff96bd6ba9ecaf4eabbb
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2020-06-12 21:20:14 +0200

    Implement enabling wifi client and connecting to stored ssid and pass

--- main/wifi.c
+++ main/wifi.c
@@ -8,6 +8,8 @@ static const char *TAG = "WiFi";
 
 wifi_state_t current_state = WIFI_DISABLED;
 
+static int s_retry_num = 0;
+
 static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
     if (event_id == WIFI_EVENT_AP_STACONNECTED) {
         wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
@@ -15,6 +17,21 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t e
     } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
         wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
         ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d", MAC2STR(event->mac), event->aid);
+    }
+
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < 5) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:%s", ip4addr_ntoa(&event->ip_info.ip));
+        s_retry_num = 0;
     }
 }
 
@@ -33,6 +50,24 @@ void wifi_init_softap() {
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+void wifi_init_sta() {
+    wifi_config_t wifi_config = {
+        .sta = {
+            .pmf_cfg = {
+                .capable = true,
+                .required = false
+            },
+        },
+    };
+
+    memcpy(wifi_config.sta.ssid, settings.wifi_ssid_client, strlen((char*) settings.wifi_ssid_client));
+    memcpy(wifi_config.sta.password, settings.wifi_pass_client, strlen((char*) settings.wifi_pass_client));
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
 }
 
 void wifi_init() {
@@ -57,7 +92,8 @@ void wifi_set_state(wifi_state_t state) {
         }
     } else if (state == WIFI_CLIENT) {
         if (current_state != WIFI_CLIENT) {
-            wifi_init_softap();
+            wifi_init_sta();
+            
             current_state = WIFI_CLIENT;
         }
     } else {
